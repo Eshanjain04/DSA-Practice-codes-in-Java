bc2f5338d0f0f0810aae331fbf48a414e1a79dfd
Eshanjain04 <65233685+Eshanjain04@users.noreply.github.com>
2021-09-28 21:54:58 +0530

    Rope Cutting problem for Max cuts

---
+++ Rope cut for Max pieces.java
@@ -0,0 +1,23 @@
+public class HelloWorld{
+    
+    
+    static int maxPiece(int l,int a,int b,int c){
+        if(l==0){
+            return 0;
+        }
+        if(l<0){
+            return -1;
+        }
+        int res = Math.max(Math.max(maxPiece(l-a,a,b,c),maxPiece(l-b,a,b,c)),maxPiece(l-c,a,b,c));
+        if(res==-1){
+            return -1;
+        }
+        return res+1;
+    }
+
+     public static void main(String []args){
+        //System.out.println("Hello World");
+        System.out.println(maxPiece(23,11,9,12));
+        
+     }
+}
